9e202becea64a79ef3fb5c5b5e6aa854832109c9
DavideBenzo <d.benzoni@lswr.it>
2024-06-18 09:30:36 +0200

    rename kr to k

--- resources/views/components/cms-custom-navbar/search-sidebar-input.blade.php
+++ resources/views/components/cms-custom-navbar/search-sidebar-input.blade.php
@@ -5,7 +5,7 @@
     </span>
     <div class="relative w-full">
        
-        <input type="text" id="kr" name="kr" value="{{$searchKey}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-brand-500 focus:border-brand-500 block w-full ps-10   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-brand-500 dark:focus:border-brand-500" placeholder="" required />
+        <input type="text" id="k" name="k" value="{{$searchKey}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-brand-500 focus:border-brand-500 block w-full ps-10   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-brand-500 dark:focus:border-brand-500" placeholder="" required />
         <button type="submit" class="absolute inset-y-0 end-0 flex items-center pe-3">
         <svg class="w-4 h-4 me-2 text-brand-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
