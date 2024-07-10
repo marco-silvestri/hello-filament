6e09946a48b009784c7a85a6e5240500e3956089
marco-silvestri <m.silvestri@lswr.it>
2024-07-10 15:16:52 +0200

    fix guard in prod

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use App\Enums\RoleEnum;
 use Filament\Panel;
 use App\Models\Post;
+use App\Enums\RoleEnum;
 use App\Models\Profile;
 use App\Traits\Cms\HasSlug;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -15,7 +16,7 @@ use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, HasRoles, SoftDeletes, HasSlug;
 
