301ca8bf3204004cd1d108b4185800ed6042b195
DavideBenzo <d.benzoni@lswr.it>
2024-08-28 10:40:08 +0200

    css up image

--- resources/views/components/elements/up.blade.php
+++ resources/views/components/elements/up.blade.php
@@ -1,11 +1,11 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" class="stroke-brand-500 hover:stroke-brand-700 fixed z-50 text-lg font-semibold text-white transition-colors duration-300 border-0 shadow-md bottom-10 right-10 w-14 h-14">
+<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" class="fill-brand-500 stroke-brand-500  hover:fill-brand-700 hover:stroke-brand-700 fixed z-50 text-lg font-semibold text-white transition-colors duration-300 border-0 shadow-md bottom-10 right-10 w-14 h-14">
   <g id="up" transform="translate(-975 -362)">
-    <g id="Rettangolo_130" data-name="Rettangolo 130" transform="translate(975 362)" fill="none" stroke="#d8000c" stroke-width="2">
+    <g id="Rettangolo_130" data-name="Rettangolo 130" transform="translate(975 362)" fill="none" stroke-width="2">
       <rect width="40" height="40" stroke="none"/>
       <rect x="1" y="1" width="38" height="38" fill="none"/>
     </g>
     <g id="Componente_50_1" data-name="Componente 50 – 1" transform="translate(987 374)">
-      <path id="Tracciato_10" data-name="Tracciato 10" d="M8,0,6.545,1.455l5.506,5.506H0V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(0 16) rotate(-90)" fill="#d8000c"/>
+      <path id="Tracciato_10" data-name="Tracciato 10" d="M8,0,6.545,1.455l5.506,5.506H0V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(0 16) rotate(-90)" />
     </g>
   </g>
 </svg>
