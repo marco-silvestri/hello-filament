1a9b7c763a17d5c27701f69bafa69a239fb378e6
marco-silvestri <m.silvestri@lswr.it>
2024-09-02 17:04:45 +0200

    revised listing

--- resources/views/components/sections/listing-deck.blade.php
+++ resources/views/components/sections/listing-deck.blade.php
@@ -1,24 +1,35 @@
 @php
-$firstHalf=[];
-$secondHalf=[];
-foreach($section as $post){
-    if (count($firstHalf)<9){
-    $firstHalf[]=$post;
-    } else {
-        $secondHalf[]=$post;
+    $firstHalf = [];
+    $secondHalf = [];
+    foreach ($section as $post) {
+        if (count($firstHalf) < 9) {
+            $firstHalf[] = $post;
+        } else {
+            $secondHalf[] = $post;
+        }
     }
-}
 @endphp
-@if($section)
+@if ($section)
     <section>
         <div class="justify-between md:flex md:flex-col">
             <div class="flex flex-col md:flex-row">
                 <div class="flex flex-col w-full md:w-3/4">
                     <div class="grid grid-cols-1 md:grid-cols-3">
-
                         @foreach ($firstHalf as $post)
                             <x-cards.simple-card :post="$post" />
                         @endforeach
+                        @foreach ($secondHalf as $post)
+                            <x-cards.simple-card :post="$post" />
+                        @endforeach
+                    </div>
+                    <div class="flex flex-col md:flex-row">
+                        <div class="flex flex-col w-full md:w-3/4">
+                            @if ($section->total() > $section->perPage())
+                                <div class="mb-4">
+                                    {{ $section->onEachSide(1)->links() }}
+                                </div>
+                            @endif
+                        </div>
                     </div>
                 </div>
                 <div class="w-full md:w-1/4">
@@ -32,23 +43,21 @@ foreach($section as $post){
                     </div>
                     <div class="w-full">
                         <x-cms-custom-navbar.search-sidebar-input :searchKey="$searchKey ?? ''" />
-
                     </div>
-
                 </div>
             </div>
-            @if (count($secondHalf)>0)
-            <x-ads.masthead :n="1" />
-            <?/************************* */?>
-            <div class="flex flex-col md:flex-row>
+            @if (count($secondHalf) > 0)
+                <x-ads.masthead :n="1" />
+
+                <div class="flex flex-col md:flex-row>
                 <div class="flex flex-col w-full md:w-3/4">
-                    <div class="grid grid-cols-1 md:grid-cols-3">
+                    {{-- <div class="grid grid-cols-1 md:grid-cols-3">
 
                         @foreach ($secondHalf as $post)
                             <x-cards.simple-card :post="$post" />
                         @endforeach
 
-                    </div>
+                    </div> --}}
                 </div>
                 <div class="w-full md:w-1/4">
                     <div class="w-full">
@@ -57,23 +66,8 @@ foreach($section as $post){
                         </div>
                     </div>
                 </div>
-            </div>
-           @endif
-           <div class="flex flex-col md:flex-row">
-           <div class="flex flex-col w-full md:w-3/4">
-           @if ($section->total() > $section->perPage())
-                <div class="mb-4">
-                    {{$section->onEachSide(1)->links()}}
-                </div>
-            @endif
-            </div>
-            </div>
-            <?/********************* */ ?>
         </div>
-    </section>
 @endif
-
-
-
-
-               
+</div>
+</section>
+@endif
