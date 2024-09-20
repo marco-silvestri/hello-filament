20dcd18b984e6b69c7e62bad605bbe12b88c15ce
Alban Delishi <a.delishi@lswr.it>
2024-09-20 13:43:23 +0000

    Merged in feat/add-tag-tracking-visits (pull request #47)
    Feat/add tag tracking visits
    
    * Refactor TrackVisits middleware to track visits and handle termination
    
    * restore Trak Visits
    
    * add tracking visits for tag and category
    
    * Refactor TrackVisit middleware to track visits based on route names
    
    * Refactor TrackVisits middleware to track visits based on slug
    
    * remove indent
    
    * Refactor TrackVisit middleware to track visits based on route names
    
    
    Approved-by: Marco Silvestri

--- app/Listeners/Cms/TrackVisit.php
+++ app/Listeners/Cms/TrackVisit.php
@@ -4,7 +4,9 @@ namespace App\Listeners\Cms;
 
 use Exception;
 use App\Models\Post;
+use App\Models\Slug;
 use App\Models\Visit;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use App\Events\Cms\LandingOnContent;
 use Illuminate\Queue\InteractsWithQueue;
@@ -29,18 +31,50 @@ class TrackVisit implements ShouldQueue
         try {
             $slug = $event->slug;
 
-            $post = Post::whereHas('slug', function (Builder $query)  use ($slug) {
-                $query->where('name', $slug);
-            })->first();
-            if ($post) {
-                $now = now();
-                $post->visits()->create([
-                    'user_id' => $event->userId,
-                    'created_at' => $now,
-                ]);
+            if (request()->routeIs('category') || request()->routeIs('tag')) {
+                $visitsId = $this->insertVisits($event);
+                $sluggable = $this->getSlugName($slug);
+                $this->insertVisitables($visitsId, $sluggable);
+            } else {
+                $post = Post::whereHas('slug', function (Builder $query)  use ($slug) {
+                    $query->where('name', $slug);
+                })->first();
+                if ($post) {
+                    $now = now();
+                    $post->visits()->create([
+                        'user_id' => $event->userId,
+                        'created_at' => $now,
+                    ]);
+                }
             }
         } catch (Exception $e) {
             Log::error("Cannot track visit: $event->slug" . PHP_EOL . "Because {$e->getMessage()}");
         }
+    }
+
+    private function insertVisits($event): int
+    {
+        $visit = Visit::create([
+            'user_id' => $event->userId,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        return $visit->id;
+    }
+
+    private function getSlugName($slug): Slug
+    {
+        $slug = basename($slug);
+        return Slug::where('name', $slug)->first();
+    }
+
+    private function insertVisitables($visitsId, $sluggable): void
+    {
+        DB::table('visitables')->insert([
+            'visit_id' => $visitsId,
+            'visitable_type' => $sluggable->sluggable_type,
+            'visitable_id' => $sluggable->sluggable_id,
+        ]);
     }
 }
