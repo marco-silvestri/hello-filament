6838e56029c9623adc5d4d42c7e30370a8e24eaf
Samuel <s.bellamacina@lswr.it>
2025-01-28 11:27:22 +0100

    Remove unused code that causes problems when saving a post with an empty paragraph

--- app/Filament/Resources/PostResource/Pages/EditPost.php
+++ app/Filament/Resources/PostResource/Pages/EditPost.php
@@ -117,31 +117,10 @@ class EditPost extends EditRecord
     {
         $data['content'] = HasJsonOperations::extractMeaningfulContent($data['json_content']);
         $record->update($data);
+
         $key = $record->getTable();
         Cache::forget("{$key}-{$record->slug}");
 
-        foreach ($data['json_content'] as $key => $block) {
-            if ($block['type'] == 'paragraph') {
-
-                // Crea un nuovo oggetto DOMDocument
-                $dom = new DOMDocument();
-
-                // Carica l'HTML
-                $dom->loadHTML($block['data']['content']);
-
-                // Seleziona tutti i tag <img>
-                $images = $dom->getElementsByTagName('img');
-
-                // Itera su ogni tag <img> trovato
-                foreach ($images as $image) {
-                    // Recupera il valore dell'attributo src
-                    $src = $image->getAttribute('src');
-
-                    // dd($src);
-                    // dd(Storage::disk('public')->size('media/5cc8b5f9-43d1-4234-9419-36a523a0347a.png'));
-                }
-            }
-        }
         return $record;
     }
 
