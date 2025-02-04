59b82a616f1c0d502ac05e0b7a99255ec3651d2d
Samuel <s.bellamacina@lswr.it>
2025-02-04 14:41:05 +0100

    Fix card layout and anchors

--- resources/views/components/cards/highlight-sm-card.blade.php
+++ resources/views/components/cards/highlight-sm-card.blade.php
@@ -3,29 +3,25 @@
     'group' => null,
     'shownNumber' => null,
 ])
-<section>
-    <div class="flex items-center w-full mt-4 mb-4 mr-4">
-        @if($post->slug)
+<div class="flex items-center mt-4 mb-4">
+    <div class="basis-[85px] mr-4 relative">
         <a href="{{ $post->url() }}">
-        @else
-        <a href="{{route('home')}}">
-        @endif
-            <div class="basis-[85px] mr-4 relative">
-                <x-curator-glider class="object-cover rounded-md" :media="$post->featuredImage" fit="crop-center" format="webp"
-                    height="85" width="85" fallback="article_fallback" />
-                    @if($shownNumber)
-                    <div class="absolute bottom-0 right-0 px-2 pt-1 text-lg font-black bg-white rounded-tl-md text-brand-500">
-                        {{$shownNumber}}
-                    </div>
-                    @endif
-            </div>
-            <div class="basis-2/3">
-                <x-elements.group-title :title="$group" />
-                <h3 class="pb-2 card--title card--title__small">
-                    {{ $post->title }}
-                </h3>
-                <x-elements.card-meta-info :post="$post" />
-            </div>
+            <x-curator-glider class="object-cover rounded-md" :media="$post->featuredImage" fit="crop-center" format="webp"
+                height="85" width="85" fallback="article_fallback" />
         </a>
+        @if($shownNumber)
+            <div class="absolute bottom-0 right-0 px-2 pt-1 text-lg font-black bg-white rounded-tl-md text-brand-500">
+                {{$shownNumber}}
+            </div>
+        @endif
     </div>
-</section>
+    <div class="basis-2/3">
+        <x-elements.group-title :title="$group" />
+        <h3 class="pb-2 card--title card--title__small">
+            <a href="{{ $post->url() }}">
+                {{ $post->title }}
+            </a>
+        </h3>
+        <x-elements.card-meta-info :post="$post" />
+    </div>
+</div>
