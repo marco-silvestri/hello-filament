a0a739697a1aed4b70da7487314fc4878bf77d1d
marco-silvestri <m.silvestri@lswr.it>
2025-02-04 11:19:00 +0100

    applied layout patch

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="application-name" content="{{ config('app.name') }}">
@@ -8,15 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @include('components.elements.favicon')
 
-    <meta name="description" content="{{__('meta-tags.description')}}">
-    <meta property="og:title" content="{{__('meta-tags.title')}}">
-    <meta property="og:description" content="{{__('meta-tags.description')}}">
-    <meta property="og:site_name" content="{{config('app.name')}}">
-    <meta property="og:image" content="{{__('meta-tags.image')}}">
+    @section('meta-tags')
+        <meta name="description" content="{{__('meta-tags.description')}}">
+        <meta property="og:title" content="{{__('meta-tags.title')}}">
+        <meta property="og:description" content="{{__('meta-tags.description')}}">
+        <meta property="og:site_name" content="{{config('app.name')}}">
+        <meta property="og:image" content="{{__('meta-tags.image')}}">
+    @show
 
     @section('title')
-    <title>{{ config('app.name') }}</title>
+        <title>{{ config('app.name') }}</title>
     @show
+
     <style>
         [x-cloak] {
             display: none !important;
@@ -26,9 +28,11 @@
     @filamentStyles
     @livewireStyles
     @vite('resources/css/app.css')
+    {!! $headSnippets !!}
 </head>
-
 <body class="antialiased">
+    {!! $bodySnippets !!}
+
     <div class="md:w-[1280px] m-auto">
         {{ $slot }}
     </div>
@@ -36,6 +40,6 @@
     @filamentScripts
     @livewireScripts
     @vite('resources/js/app.js')
+    {!! $footerSnippets !!}
 </body>
-
 </html>
